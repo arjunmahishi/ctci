51fa3f05389abac213c0ef4da22e745d9a536b2a
Arjun Mahishi <arjun.mahishi@gmail.com>
2019-06-03 18:03:01 +0530

    Generating docs/README for github-pages

--- scripts/updateIndex.js
+++ scripts/updateIndex.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const INDENT_CHAR = "\t";
 
 var indexString = "# Index\n\n";
+var docString = "";
 
 const getIndent = (n) => {
 	indent = "";
@@ -12,20 +13,30 @@ const getIndent = (n) => {
 	return indent;
 };
 
+const getReadmeText = (path) => {
+	let conts = fs.readFileSync(path)	
+	return conts.toString()
+}
+
 const walk = (dir, level=0) => {
-	let files = fs.readdirSync(dir).filter(file => 
+	let allFiles = fs.readdirSync(dir)
+	let files = allFiles.filter(file => 
 		fs.statSync(dir + "/" + file).isDirectory()
 	);
 	files.forEach(file => {
 		let filePath = `${dir}/${file}`;
 		let link = filePath.replace("./", "");
 		indexString += `${getIndent(level)}- [${file}](${link})\n`;
+		if (fs.existsSync(`${dir}/${file}/README.md`)) {
+			docString += `# ${file}\n${getReadmeText(`${dir}/${file}/README.md`)}\n`
+		}
 		walk(filePath, level+1);
 	});
 };
 
 const build = () => {
 	walk("./problems");
+
 	fs.writeFile("./INDEX.md", indexString, (err) => {
 		if(err) {
 			console.log("couldn't write to INDEX.md. err:", err);
@@ -33,13 +44,15 @@ const build = () => {
 			console.log("INDEX.md successfully updated!");
 		}
 	});
-    fs.writeFile("./docs/README.md", indexString, (err) => {
-        if(err) {
-            console.log("couldn't write to INDEX.md. err:", err);
-        }else {
-            console.log("INDEX.md successfully updated!");
-        }
-    });
+
+	fs.writeFile("./docs/README.md", docString, (err) => {
+		if(err) {
+			console.log("couldn't write to docs/README.md err:", err);
+		}else {
+			console.log("docs/README.md successfully updated!");
+		}
+	});
 };
 
 build();
+// getReadmeText("./problems/other/README.md")
